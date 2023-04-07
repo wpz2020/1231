1bab9f3dbbfbfd62f82bab7be35dc35e8afce88b
JamesYu <115390358+Yutian2000@users.noreply.github.com>
2023-04-07 19:23:03 +0800

    Update home.tsx

--- app/components/home.tsx
+++ app/components/home.tsx
@@ -231,11 +231,7 @@ export function Home() {
                 shadow
               />
             </div>
-            <div className={styles["sidebar-action"]}>
-              <a href={REPO_URL} target="_blank">
-                <IconButton icon={<GithubIcon />} shadow />
-              </a>
-            </div>
+ 
           </div>
           <div>
             <IconButton
